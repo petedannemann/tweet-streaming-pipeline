26518dd7c86e80392f1582b298b3358729fcb6c5
Peter <pbd281@gmail.com>
2021-04-11 09:31:57 -0400

    provide fake env vars in test

--- test_producer.py
+++ test_producer.py
@@ -1,4 +1,5 @@
 import base64
+import os
 import unittest
 
 import boto3
@@ -8,6 +9,13 @@ import moto.backends
 import producer
 
 ACCOUNT_ID = "fake-account-id"
+STREAM_NAME = "stream1"
+
+os.environ["STREAM_NAME"] = STREAM_NAME
+os.environ["TWITTER_API_KEY"] = "fake-api-key"
+os.environ["TWITTER_API_SECRET_KEY"] = "fake-api-secret-key"
+os.environ["TWITTER_ACCESS_TOKEN"] = "fake-access-token"
+os.environ["TWITTER_ACCESS_TOKEN_SECRET"] = "fake-access-token-secret"
 
 
 def create_s3_delivery_stream(client, stream_name):
@@ -26,7 +34,7 @@ def create_s3_delivery_stream(client, stream_name):
 class ProducerTest(unittest.TestCase):
     def setUp(self):
         self.firehose_client = boto3.client("firehose", "us-east-1")
-        self.stream_name = "stream1"
+        self.stream_name = STREAM_NAME
         create_s3_delivery_stream(self.firehose_client, self.stream_name)
         self.listener = producer.MyStreamListener(self.stream_name)
 
@@ -35,9 +43,9 @@ class ProducerTest(unittest.TestCase):
         data = "{\"text\":\"my great tweet\"}"
         self.listener.on_data(data)
         streams = self.firehose_client.list_delivery_streams()["DeliveryStreamNames"]
-        self.assertEqual(["stream1"], streams)
+        self.assertEqual([STREAM_NAME], streams)
 
-        firehose_records = moto.backends.get_backend("kinesis")["us-east-1"].delivery_streams["stream1"].records
+        firehose_records = moto.backends.get_backend("kinesis")["us-east-1"].delivery_streams[STREAM_NAME].records
         self.assertEqual(1, len(firehose_records))
         self.assertEqual(data, base64.b64decode(firehose_records[0].record_data).decode("utf-8"))
         
