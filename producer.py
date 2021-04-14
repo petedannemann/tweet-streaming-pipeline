53e56b8302a1401d993959021057c08ca13aea24
Peter <pbd281@gmail.com>
2021-04-14 18:09:20 -0400

    finish tests and small refactor

--- producer.py
+++ producer.py
@@ -13,6 +13,9 @@ import tweepy
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+# 500 records is the limit
+# https://docs.aws.amazon.com/firehose/latest/dev/limits.html
+AWS_REGION = os.environ.get("AWS_REGION", "us-east-1")
 DEFAULT_BUFFER_SIZE = int(os.environ.get("DEFAULT_BUFFER_SIZE", "500"))
 KINESIS_RETRY_COUNT = 10
 KINESIS_RETRY_WAIT_IN_SEC = 0.1
@@ -28,40 +31,50 @@ class MyStreamListener(tweepy.StreamListener):
     def __init__(
         self,
         stream_name: str,
-        region: typing.Optional[str] = 'us-east-1',
+        region: str,
     ) -> None:
         super().__init__()
-        self.firehose_client = boto3.client('firehose', region)
+        self.firehose_client = boto3.client("firehose", region)
         self.stream_name = stream_name
         self._buffer = []
+
+    def _publish_to_kinesis(
+        self, 
+        kinesis_records: typing.List[typing.Dict[str, bytes]]
+    ) -> typing.Dict[str, typing.Any]:
+        return self.firehose_client.put_record_batch(
+            DeliveryStreamName=self.stream_name,
+            Records=kinesis_records,
+        )
 
     def _flush(
         self,
         kinesis_records: typing.List[typing.Dict[str, bytes]],
         retry_count: typing.Optional[int] = KINESIS_RETRY_COUNT,
     ) -> None:
-        put_response = self.firehose_client.put_record_batch(
-            DeliveryStreamName=self.stream_name,
-            Records=kinesis_records,
-        )
-
-        failed_count = put_response['FailedPutCount']
+        put_response = self._publish_to_kinesis(kinesis_records)
+        failed_count = put_response["FailedPutCount"]
 
         if failed_count > 0:
             if retry_count > 0:
                 retry_kinesis_records = []
-                for idx, record in enumerate(put_response['Records']):
-                    if 'ErrorCode' in record:
+                for idx, record in enumerate(put_response["Records"]):
+                    if "ErrorCode" in record:
                         retry_kinesis_records.append(kinesis_records[idx])
                 time.sleep(KINESIS_RETRY_WAIT_IN_SEC * (KINESIS_RETRY_COUNT - retry_count + 1))
                 self._flush(retry_kinesis_records, retry_count - 1)
             else:
                 logger.error("Not able to put records after retries. Records: %s", put_response["Records"])
-        logger.info("Flushed tweets to kinesis firehose")
+        else:
+            logger.info("Successfully wrote tweets to kinesis firehose")
+
+    def close(self):
+        if len(self._buffer) > 0:
+            self._flush(self._buffer)
 
     def on_data(self, data: bytes) -> bool:
         logger.debug("Received record: %s", json.dumps(data))
-        self._buffer.append({'Data': data})
+        self._buffer.append({"Data": data})
 
         if len(self._buffer) >= DEFAULT_BUFFER_SIZE:
             self._flush(self._buffer)
@@ -75,17 +88,16 @@ class MyStreamListener(tweepy.StreamListener):
     def on_error(self, status_code) -> bool:
         if status_code == 420:
             logging.warn("Rate limit exceeded.")
-            return True
-
-        logger.error("Got an error with status code: %i - restarting the stream", status_code)
+        else:
+            logger.error("Got an error with status code: %i - restarting the stream", status_code)
         # returning False disconnects the stream
         return True
 
 
-def main(stream_name: str) -> None:
+def main() -> None:
     auth = tweepy.OAuthHandler(TWITTER_API_KEY, TWITTER_API_SECRET_KEY)
     auth.set_access_token(TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_TOKEN_SECRET)
-    stream_listener = MyStreamListener(stream_name)
+    stream_listener = MyStreamListener(STREAM_NAME, AWS_REGION)
 
     while True:
         try:
@@ -99,7 +111,9 @@ def main(stream_name: str) -> None:
                 os._exit(0)
         except Exception as ex:
             logging.error("Stream would have died, caught exception", exc_info=ex)
+        finally:
+            stream_listener.close()
 
 
 if __name__ == '__main__':
-    main(STREAM_NAME)
+    main()
--- test_producer.py
+++ test_producer.py
@@ -1,6 +1,7 @@
 import base64
 import os
 import unittest
+from unittest import mock
 
 import boto3
 import moto
@@ -37,24 +38,50 @@ class ProducerTest(unittest.TestCase):
         self.firehose_client = boto3.client("firehose", "us-east-1")
         self.stream_name = STREAM_NAME
         create_s3_delivery_stream(self.firehose_client, self.stream_name)
-        self.listener = producer.MyStreamListener(self.stream_name)
+        self.listener = producer.MyStreamListener(self.stream_name, "us-east-1")
 
+    def test_stream_exists(self):
+        streams = self.firehose_client.list_delivery_streams()["DeliveryStreamNames"]
+        self.assertEqual([STREAM_NAME], streams)
 
     def test_on_data(self):
         data = "{\"text\":\"my great tweet\"}"
 
         for _ in range(5):
             self.listener.on_data(data)
-        
-        streams = self.firehose_client.list_delivery_streams()["DeliveryStreamNames"]
-        self.assertEqual([STREAM_NAME], streams)
 
         firehose_records = moto.backends.get_backend("kinesis")["us-east-1"].delivery_streams[STREAM_NAME].records
+        # One record is still in the buffer and hasn't been published
         self.assertEqual(4, len(firehose_records))
         self.assertEqual([data] * 4, [base64.b64decode(record.record_data).decode("utf-8") for record in firehose_records])
 
+        # Close should flush any outstanding records
+        self.listener.close()
+        self.assertEqual(5, len(firehose_records))
+
     def test_on_data_retry(self):
-        self.assertTrue(False)
+        data = "{\"text\":\"my great tweet\"}"
+
+        self.listener.on_data(data)
+
+        all_records = [data] * 2
+        error_records = [{"ErrorCode": "some error", "Data": rec} for rec in all_records]
+
+        with mock.patch.object(
+            self.listener,
+            '_publish_to_kinesis',
+            side_effect=[
+                {"FailedPutCount": 2, "Records": error_records},
+                {"FailedPutCount": 1, "Records": error_records[:2]},
+                {"FailedPutCount": 0},
+            ],
+        ) as put_mock:
+            self.listener.on_data(data)
+            record_data = [{"Data": rec} for rec in all_records]
+            put_mock.assert_has_calls([
+                mock.call(record_data),
+                mock.call(record_data[:2])
+            ])
         
 
 if __name__ == "__main__":
